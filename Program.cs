859da09b2bbfd93e032f0d6378d53cdc08951cfe
rodrigocamperopaz <rodrigo.campero@trueforce.com>
2022-04-20 15:56:47 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MathServiceConsumer
+{
+
+    public static class Program
+    {
+        public static void Main()
+        {   
+            var mathServiceSoap = new MathService.MathServiceSoapClient(MathService.MathServiceSoapClient.EndpointConfiguration.MathServiceSoap12);
+
+            Console.WriteLine("2 + 4 = {0}", mathServiceSoap.Add(2, 4));
+            Console.WriteLine("6 - 4 = {0}", mathServiceSoap.Subtract(6, 4));
+            Console.WriteLine("2 * 4 = {0}", mathServiceSoap.Multiply(2, 4));
+            Console.WriteLine("2 / 0 = {0}", mathServiceSoap.Divide(2, 0));
+        }
+    }
+}
